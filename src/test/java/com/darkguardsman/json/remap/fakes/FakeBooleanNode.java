0296be727d6b4c5fce5b323befa4677be019d320
Robin Seifert <rseifert.phone@gmail.com>
2022-12-03 12:59:32 -0500

    test: added mock boolean node for tests

---
+++ src/test/java/com/darkguardsman/json/remap/fakes/FakeBooleanNode.java
@@ -0,0 +1,10 @@
+package com.darkguardsman.json.remap.fakes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class FakeBooleanNode {
+    private Boolean value;
+}
