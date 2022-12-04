94485201802597bc89ff0569d3355ee9ee056869
Robin Seifert <rseifert.phone@gmail.com>
2022-12-03 21:55:18 -0500

    test: added tests for array split logic

--- src/test/java/com/darkguardsman/json/remap/mappers/ArraySplitMapperTest.java
+++ src/test/java/com/darkguardsman/json/remap/mappers/ArraySplitMapperTest.java
@@ -9,9 +9,12 @@ import com.darkguardsman.json.remap.fakes.FakeStringNode;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ArraySplitMapperTest {
@@ -60,5 +63,109 @@ public class ArraySplitMapperTest {
         // Validate our starting settings
         Assertions.assertEquals("; ", newMapper.getSplitString());
         Assertions.assertTrue(newMapper.isDoTrim());
+    }
+
+    @Test
+    @DisplayName("Turns empty string into empty array")
+    void handleEmptyString() {
+
+        // Setup data
+        final FakeStringNode textNode = new FakeStringNode("");
+        final String[] returnArray = new String[0];
+
+        // Mock handler
+        final INodeHandler handler = Mockito.mock(INodeHandler.class);
+        Mockito.when(handler.isNull(textNode)).thenReturn(false);
+        Mockito.when(handler.isEmpty(textNode)).thenReturn(true);
+        Mockito.when(handler.newArray()).thenReturn(returnArray);
+
+        // Setup and invoke mapper
+        final ArraySplitMapper mapper = new ArraySplitMapper(";", false);
+        final Object result = mapper.apply(handler, textNode);
+
+        // Check that we get an empty array
+        Assertions.assertEquals(returnArray, result);
+    }
+
+    @Test
+    @DisplayName("Returns null when given null")
+    void handleNull() {
+
+        // Setup data, using raw object as a placeholder for a null node
+        final Object nullNode = new Object();
+
+        // Mock handler
+        final INodeHandler handler = Mockito.mock(INodeHandler.class);
+        Mockito.when(handler.isNull(nullNode)).thenReturn(true);
+        Mockito.when(handler.newNull()).thenReturn(null);
+
+        // Setup and invoke mapper
+        final ArraySplitMapper mapper = new ArraySplitMapper(";", false);
+        final Object result = mapper.apply(handler, nullNode);
+
+        // Check that we get null
+        Assertions.assertNull(result);
+    }
+
+    @Test
+    @DisplayName("Converts 'abc,3fg, 890' into ['abc', '3fg', ' 890']")
+    void splitsString() {
+
+        // Setup data, using raw object as a placeholder for a null node
+        final FakeStringNode textNode = new FakeStringNode("abc,3fg, 890");
+
+        // Mock handler
+        final INodeHandler handler = Mockito.mock(INodeHandler.class);
+        Mockito.when(handler.isNull(textNode)).thenReturn(false);
+        Mockito.when(handler.isEmpty(textNode)).thenReturn(false);
+        Mockito.when(handler.newArray()).thenReturn(new ArrayList());
+        Mockito.doAnswer((args) -> {
+            ((ArrayList)args.getArgument(0)).add(args.getArgument(1));
+            return null;
+        }).when(handler).addItem(Mockito.any(), Mockito.any());
+        Mockito.when(handler.asText(Mockito.any())).thenAnswer((args) -> ((FakeStringNode)args.getArgument(0)).getText());
+        Mockito.when(handler.newText(Mockito.any())).thenAnswer((args) -> new FakeStringNode(args.getArgument(0)));
+
+        // Setup and invoke mapper
+        final ArraySplitMapper mapper = new ArraySplitMapper(",", false);
+        final Object result = mapper.apply(handler, textNode);
+
+        // Check that we get null
+        Assertions.assertEquals(List.of(
+                new FakeStringNode("abc"),
+                new FakeStringNode("3fg"),
+                new FakeStringNode(" 890")
+        ), result);
+    }
+
+    @Test
+    @DisplayName("Converts 'abc,3fg, 890' into ['abc', '3fg', '890']")
+    void splitsString_doTrim() {
+
+        // Setup data, using raw object as a placeholder for a null node
+        final FakeStringNode textNode = new FakeStringNode("abc,3fg, 890");
+
+        // Mock handler
+        final INodeHandler handler = Mockito.mock(INodeHandler.class);
+        Mockito.when(handler.isNull(textNode)).thenReturn(false);
+        Mockito.when(handler.isEmpty(textNode)).thenReturn(false);
+        Mockito.when(handler.newArray()).thenReturn(new ArrayList());
+        Mockito.doAnswer((args) -> {
+            ((ArrayList)args.getArgument(0)).add(args.getArgument(1));
+            return null;
+        }).when(handler).addItem(Mockito.any(), Mockito.any());
+        Mockito.when(handler.asText(Mockito.any())).thenAnswer((args) -> ((FakeStringNode)args.getArgument(0)).getText());
+        Mockito.when(handler.newText(Mockito.any())).thenAnswer((args) -> new FakeStringNode(args.getArgument(0)));
+
+        // Setup and invoke mapper
+        final ArraySplitMapper mapper = new ArraySplitMapper(",", true);
+        final Object result = mapper.apply(handler, textNode);
+
+        // Check that we get null
+        Assertions.assertEquals(List.of(
+                new FakeStringNode("abc"),
+                new FakeStringNode("3fg"),
+                new FakeStringNode("890")
+        ), result);
     }
 }
