df1936cf750f3eb8754322d7e420651cf069a326
Robin Seifert <rseifert.phone@gmail.com>
2022-12-04 22:51:31 -0500

    feat: added assertions to accessor type get and added logic for single field lookups

--- src/main/java/com/darkguardsman/json/remap/core/accessors/AccessorGet.java
+++ src/main/java/com/darkguardsman/json/remap/core/accessors/AccessorGet.java
@@ -3,6 +3,7 @@ package com.darkguardsman.json.remap.core.accessors;
 import com.darkguardsman.json.remap.core.errors.MapperException;
 import com.darkguardsman.json.remap.core.imp.IAccessorFunc;
 import com.darkguardsman.json.remap.core.imp.INodeHandler;
+import lombok.Data;
 
 /**
  * Simple lookup using field names. Doesn't support more complex lookups using
@@ -13,6 +14,7 @@ import com.darkguardsman.json.remap.core.imp.INodeHandler;
  * Examples:
  * *
  */
+@Data
 public class AccessorGet<T, O extends T, A extends T> implements IAccessorFunc<T> {
 
     private final INodeHandler<T, O, A> handler;
@@ -26,43 +28,54 @@ public class AccessorGet<T, O extends T, A extends T> implements IAccessorFunc<T
      * @param useRoot will be true when lookup is from the root json element
      */
     public AccessorGet(INodeHandler<T, O, A> handler, String accessor, boolean useRoot) {
+        assert accessor != null && !accessor.isBlank();
+        assert handler != null;
+
         this.accessor = accessor;
         this.useRoot = useRoot;
         this.handler = handler;
-        this.fields = accessor.split("[.]");
+        this.fields = accessor.contains(".") ? accessor.split("[.]") : null;
     }
 
     @Override
     public T apply(T node, T root) {
         T value = useRoot ? root : node;
 
+        // Access single field without nesting
+        if(fields == null) {
+            return getField(value, accessor);
+        }
+
         // Dive fields in attempt to resolve full path
         for (String field : fields) {
-
-            // Validate we are an object, if not throw an error as it likely means our input is invalid or mapper is outdated
-            if (handler.isObject(value)) {
-                value = handler.getField(handler.asObject(value), field);
-                if (value == null) {
-                    return null;
-                }
-            }
-            // Get by index
-            else if(handler.isArray(value)) {
-
-                try {
-                    int index = Integer.parseInt(field);
-                    value = handler.getItem(handler.asArray(value), index);
-                }
-                catch(NumberFormatException e) {
-                    final String formattedMessage = String.format("Failed to access '%s' as field '%s' was not an index for the array found, node=%s", accessor, field, value);
-                    throw new MapperException(formattedMessage);
-                }
-            }
-            else {
-                final String formattedMessage = String.format("Failed to access '%s' as field '%s' was not an object, node=%s", accessor, field, value);
-                throw new MapperException(formattedMessage);
+            value = getField(value, field);
+            if(value == null) {
+                return handler.newNull();
             }
         }
         return value;
+    }
+
+    private T getField(T value, String field) {
+        // Validate we are an object, if not throw an error as it likely means our input is invalid or mapper is outdated
+        if (handler.isObject(value)) {
+            return handler.getField(handler.asObject(value), field);
+        }
+        // Get by index
+        else if(handler.isArray(value)) {
+
+            try {
+                int index = Integer.parseInt(field);
+                return handler.getItem(handler.asArray(value), index);
+            }
+            catch(NumberFormatException e) {
+                final String formattedMessage = String.format("Failed to access '%s' as field '%s' was not an index for the array found, node=%s", accessor, field, value);
+                throw new MapperException(formattedMessage);
+            }
+        }
+        else {
+            final String formattedMessage = String.format("Failed to access '%s' as field '%s' was not an object, node=%s", accessor, field, value);
+            throw new MapperException(formattedMessage);
+        }
     }
 }
